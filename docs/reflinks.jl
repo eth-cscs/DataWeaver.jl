578c528ea85b0f4fe81448e6ba6b8cfcbe89e8af
Samuel Omlin <samuel.omlin@cscs.ch>
2023-08-28 14:43:16 +0200

    add doc build infrastructure

---
+++ docs/reflinks.jl
@@ -0,0 +1,5 @@
+reflinks = Dict(
+    "[ADIOS2.jl]"             => "https://github.com/eschnett/ADIOS2.jl",
+    "[Julia package manager]" => "https://docs.julialang.org/en/v1/stdlib/Pkg/",
+    "[Julia REPL]"            => "https://docs.julialang.org/en/v1/stdlib/REPL/",
+)
