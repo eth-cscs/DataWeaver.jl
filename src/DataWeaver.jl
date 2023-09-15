03db4382c349b1bd313238858dbea4d62d4fd035
Maja Gwóźdź <maja.k.gwozdz@gmail.com>
2023-09-15 14:20:50 +0200

    Rename init and finalize

--- src/DataWeaver.jl
+++ src/DataWeaver.jl
@@ -4,12 +4,12 @@ Module DataWeaver
 
 module DataWeaver
 
-export adios2_init, write_setup, write, read_setup, reading, read, finalize_adios,
+export init_data_weaver, write_setup, write, read_setup, reading, read, finalize_data_weaver,
        inspect_variables
 
 let
-    global adios2_init, write_setup, write, read_setup, reading, reading_now, read,
-    finalize_adios, adios, engine, vars, init_state, nprocessed
+    global init_data_weaver, write_setup, write, read_setup, reading, reading_now, read,
+    finalize_data_weaver, adios, engine, vars, init_state, nprocessed
 
     include("rw.jl")
     include("utils.jl")
